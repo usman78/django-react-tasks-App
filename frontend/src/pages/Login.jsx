4fa2d2df8c61278d0ec6670a6bd0984846e535e6
Usman Ali <iam.usmanali1@gmail.com>
2024-08-12 11:15:42 +0500

    Integrated django with react components

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+import Forms from "../components/Forms.jsx"
+
+function Login(){
+    return <Forms route="/api/token/" method="login"/>
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,7 @@
+import Forms from "../components/Forms.jsx"
+
+function Register(){
+    return <Forms route="/api/user/register/" method="register"/>
+}
+
+export default Register
